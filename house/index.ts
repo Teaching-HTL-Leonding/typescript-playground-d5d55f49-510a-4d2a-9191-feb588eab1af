492f45ff6eb679efcc4a034df0b8ee9ec0bb5d0b
Rainer Stropek <rainer@timecockpit.com>
2026-09-18 11:42:12 +0200

    Saved in TypeScript Playground at 2026-09-18T09:42:11.858Z

--- house/index.ts
+++ house/index.ts
@@ -1,3 +1,10 @@
 function setup() {
-    createCanvas(500, 500);
+    createCanvas(700, 700);
+    background("lightblue");
+
+    fill("red");
+    rect(75, 300, 260, 300); //rotes Haus
+    
+    fill("saddlebrown");
+    triangle(20, 300, 200, 130, 400, 300); //braunes Dach
 }
