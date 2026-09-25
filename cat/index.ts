d5ad7a1fa14e3c628112cb275745f6a90b964991
Rainer Stropek <rainer@timecockpit.com>
2026-09-25 09:01:35 +0200

    Saved in TypeScript Playground at 2026-09-25T07:01:35.127Z

---
+++ cat/index.ts
@@ -0,0 +1,11 @@
+function setup() {
+    createCanvas(600, 700);
+    background("deepskyblue"); //blue bg
+
+    // grey head
+    fill("grey");
+    circle(300, 400, 370);
+
+    // grey ears
+    triangle(150, 260, 220, 200, 200, 120);
+}
